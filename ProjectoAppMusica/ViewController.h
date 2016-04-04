5d4c5bcfbba5f5bfebcd4dfbee21639583351b8f
Ivo Santos <dash.raven@gmail.com>
2016-04-04 19:11:21 +0100

    AFNetworking installed

---
+++ ProjectoAppMusica/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectoAppMusica
+//
+//  Created by Formando Web Mobile on 04/04/16.
+//  Copyright © 2016 Formando Web Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectoAppMusica/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ProjectoAppMusica
+//
+//  Created by Formando Web Mobile on 04/04/16.
+//  Copyright © 2016 Formando Web Mobile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
