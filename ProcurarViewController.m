559cb5e4b9de6bfe0d89b601f330e0fb456d2069
dashraven <dash.raven@gmail.com>
2016-04-20 22:11:16 +0100

    Added new search system

--- ProcurarViewController.m
+++ ProcurarViewController.m
@@ -33,137 +33,33 @@
 }
 
 
-- (void)showAllSongTitlesFetched:(NSArray *)fetched {
+- (IBAction)clickedBusca:(id)sender {
     
-    NSLog(@"%@", fetched);
-    
-    NSMutableArray<Songs *> *allSongSearch = [[NSMutableArray alloc] init];
-    
-    for (NSDictionary *dAllSongSearch in fetched) {
-        
-        Songs *s = [[Songs alloc] init];
-        s.songId = dAllSongSearch[@"id"];
-        s.artist = dAllSongSearch[@"artist"];
-        s.title = dAllSongSearch[@"title"];
-        s.duration = dAllSongSearch[@"duration"];
-        s.thumbURL = dAllSongSearch[@"thumb_url"];
-        s.recent = dAllSongSearch[@"recent"];
-        s.lyrics = dAllSongSearch[@"lyrics"];
-        
-        
-        
-        [allSongSearch addObject:s];
-    }
-    
-    
-    
-    _allSongSearch = [NSArray arrayWithArray:allSongSearch];
-    
-}
-
-
-- (void)showAllSongArtistsFetched:(NSArray *)fetched {
-    
-    NSLog(@"%@", fetched);
-    
-    NSMutableArray<Songs *> *allSongSearch = [[NSMutableArray alloc] init];
-    
-    for (NSDictionary *dAllSongSearch in fetched) {
-        
-        Songs *s = [[Songs alloc] init];
-        s.songId = dAllSongSearch[@"id"];
-        s.artist = dAllSongSearch[@"artist"];
-        s.title = dAllSongSearch[@"title"];
-        s.duration = dAllSongSearch[@"duration"];
-        s.thumbURL = dAllSongSearch[@"thumb_url"];
-        s.recent = dAllSongSearch[@"recent"];
-        s.lyrics = dAllSongSearch[@"lyrics"];
-        
-        
-        
-        [allSongSearch addObject:s];
-    }
-    
-    
-    
-    _allSongSearch = [NSArray arrayWithArray:allSongSearch];
-    
-}
-
-
-
-- (IBAction)clickedSegment:(id)sender {
     UISegmentedControl *segmentedControl = (UISegmentedControl *) sender;
     NSInteger selectedSegment = segmentedControl.selectedSegmentIndex;
     
+    NSMutableString *_json = [[NSMutableString alloc] initWithString:@"http://reality6.com/musicservicejson.php?tipo="];
     
+   
     if (selectedSegment == 0) {
-        
-        Songs *a = [[Songs alloc] init];
-        a.artist = self.textProcurar.text;
-        
-        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-//        NSString *url = [NSString stringWithFormat:@"http://reality6.com/musicservicejson.php?tipo=buscacancao&nome=%@", self.textProcurar.text];
-
-        NSDictionary *params = @{
-                                 @"tipo": @"buscacancao",
-                                 @"nome": self.textProcurar.text
-                                 };
-        
-        [manager GET:@"http://reality6.com/musicservicejson.php"
-          parameters:params
-            progress:nil
-             success:^(NSURLSessionDataTask *task, id responseObject) {
-                 
-                 NSLog(@"download!!!!");
-                 NSLog(@"%@", responseObject);
-                
-                 
-                 [self showAllSongTitlesFetched:responseObject];
-                 
-             }
-             failure:^(NSURLSessionDataTask *task, NSError *error) {
-                 
-                 NSLog(@"erro!!!!");
-                 NSLog(@"%@", error);
-                 
-             }
-         ];
-        
+        [_json appendString:@"buscaartista&nome="];
     } else {
-        
-        Songs *t = [[Songs alloc] init];
-        t.title = self.textProcurar.text;
-        
-        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-        //NSString *url = [NSString stringWithFormat:@"http://reality6.com/musicservicejson.php?tipo=buscaartista&nome=%@", self.textProcurar.text];
-        
-        NSDictionary *params = @{
-                                 @"tipo": @"buscaartista",
-                                 @"nome": self.textProcurar.text
-                                 };
-        
-        [manager GET:@"http://reality6.com/musicservicejson.php"
-          parameters:params
-            progress:nil
-             success:^(NSURLSessionDataTask *task, id responseObject) {
-                 
-                 NSLog(@"download!!!!");
-                 NSLog(@"%@", responseObject);
-                 
-                 
-                 [self showAllSongArtistsFetched:responseObject];
-                 
-             }
-         
-             failure:^(NSURLSessionDataTask *task, NSError *error) {
-                 
-                 NSLog(@"erro!!!!");
-                 NSLog(@"%@", error);
-             }
-         ];
+        [_json appendString:@"buscacancao&nome="];
     }
+    
+    [_json appendString:_textProcurar.text];
+    
 
+    for (int i = 0; i < _json.length; i++) {
+        NSRange posicao = NSMakeRange(i, 1);
+        char letra = [_json characterAtIndex:i];
+        if (letra == ' ') {
+            [_json replaceCharactersInRange:posicao withString:@"+"];
+        }
+    }
+    
+     [self performSegueWithIdentifier:@"ProcuraToResultados" sender:_json];
+    
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -171,15 +67,8 @@
 {
     ProcurarResultadosViewController *prvc = segue.destinationViewController;
     prvc.allfoundSongs = sender;
-
-}
-
-- (IBAction)clickedBusca:(id)sender {
-
-    [self performSegueWithIdentifier:@"ProcuraToResultados" sender:_allSongSearch];
     
 }
-
 
 
 
